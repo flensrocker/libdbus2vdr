dbe38fa6a17474b4197c44010b2ac37cd118a4d8
Lars Hanisch <dvb@flensrocker.de>
2013-06-26 23:11:48 +0200

    beautify indent

--- connection.h
+++ connection.h
@@ -3,10 +3,10 @@
 
 #include "dbus2vdr.h"
 
-void     dbus2vdr_connection_new(DBus2vdrConnection *connection, int instance_id, gpointer user_data);
-void     dbus2vdr_connection_delete(DBus2vdrConnection *connection);
+void         dbus2vdr_connection_new(DBus2vdrConnection *connection, int instance_id, gpointer user_data);
+void         dbus2vdr_connection_delete(DBus2vdrConnection *connection);
 
-gboolean dbus2vdr_connection_connect(DBus2vdrConnection *connection, GBusType bus_type, DBus2vdrConnectFunc on_connect);
+gboolean     dbus2vdr_connection_connect(DBus2vdrConnection *connection, GBusType bus_type, DBus2vdrConnectFunc on_connect);
 
 gboolean     dbus2vdr_connection_is_connected(DBus2vdrConnection *connection);
 const gchar* dbus2vdr_connection_busname(DBus2vdrConnection *connection);
